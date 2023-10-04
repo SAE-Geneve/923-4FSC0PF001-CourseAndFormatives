215c13c8c19bbee33e90e49cc7e2aec2e11c3a73
StudioAlbert <account@studio-albert.com>
2023-10-04 10:55:29 +0200

    Addind map example

--- DataStructures/DataStructures.cpp
+++ DataStructures/DataStructures.cpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <iostream>
 #include <list>
+#include <map>
 #include <queue>
 #include <stack>
 #include <vector>
@@ -67,7 +68,7 @@ int main()
     }
 
     // Satck -----------------------------------------------
-    std::cout << "STACK --------------------------" << std::endl << std::endl << std::endl;
+    std::cout << " STACK -------------------------- " << std::endl << std::endl << std::endl;
     std::stack<int> datas_stack;
     for (int i = 0; i < size; i++)
     {
@@ -79,5 +80,38 @@ int main()
         std::cout << datas_stack.top() << std::endl;
         datas_stack.pop();
     }
+
+    // Map
+    std::map<char, int> data_map;
+
+    data_map.emplace('a', 10);
+    data_map.at('a');
+    int value = data_map.at(97);
+
+    std::map<std::string, int> notes_formative1;
+    notes_formative1.emplace("Anthony", 6);
+    notes_formative1.emplace("Linus", 6);
+
+
+    if (notes_formative1.find("Jayson") != notes_formative1.end())
+    {
+	    notes_formative1.at("Jayson") = 2;
+    	notes_formative1["Jayson"] = 4;
+    }
+    else
+    {
+	    notes_formative1["Jayson"] = 6;
+    }
+
+    std::cout << "Notes de Jayson" << notes_formative1["Jayson"] << std::endl;
+    std::cout << "Notes de Jayson" << notes_formative1["Anthony"] << std::endl;
+
+    for(std::pair<std::string, int> n : notes_formative1)
+    {
+        std::cout << n.first << ":" << n.second << std::endl;
+    }
+
+    // Set
+    // Dictionnaries
 
 }
